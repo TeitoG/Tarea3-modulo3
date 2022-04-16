98547b42c1c55bc2d6ce78d670c96d18acd9e383
teo <teito206@gmail.com>
2022-04-15 20:03:06 -0500

    Tarea3

---
+++ data/data.js
@@ -0,0 +1,28 @@
+export const personajes = [{
+    nombre :"Harry Potter",
+    casa :"Gryffindor",
+    foto : "https://static.wikia.nocookie.net/hitlerparody/images/4/4a/Harry-potter-ficcion.jpg/revision/latest?cb=20201103180440&path-prefix=es",
+},{
+    nombre : "Hermione Granger",
+    casa : "Gryffindor",
+    foto : "https://static.wikia.nocookie.net/warnerbros/images/3/3e/Hermione.jpg/revision/latest?cb=20120729103114&path-prefix=es",
+},{
+    nombre : "Ron Weasley",
+    casa : "Gryffindor",
+    foto : "https://static.wikia.nocookie.net/esharrypotter/images/3/34/P7_Rony_promo.jpg/revision/latest?cb=20180719012207"
+},{
+    nombre : "Draco Malfoy",
+    casa : "Slytherin",
+    foto : "https://static.wikia.nocookie.net/esharrypotter/images/6/67/Draco_Duelo.jpg/revision/latest/scale-to-width-down/774?cb=20180501172548",
+},{
+    nombre : "Minerva McGonagall",
+    casa : "Gryffindor",
+    foto : "https://static.wikia.nocookie.net/9dce102d-d1e2-4ff6-9a7d-a96c5be37183/scale-to-width/755",
+
+},{
+    nombre : "Lord Voldermort",
+    casa : "Slytherin",
+    foto : "https://static.wikia.nocookie.net/esharrypotter/images/1/1c/Tomdh.jpg/revision/latest?cb=20110711204536",
+}
+
+]
---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+import {personajes} from "../data/data.js";
+let btnPers = document.getElementById('btnAleatorio')
+btnPers.addEventListener('click', persRandom)
+let btnPers2 = document.getElementById('btnAleatorio2')
+btnPers2.addEventListener('click', persRandom)
+
+function persRandom() {
+    let min = 0;
+    let max = 6
+    let random = min + Math.floor(Math.random() * max)
+    let Pers = personajes[random]
+    Pers = Object.values(Pers)
+    let nombre = Pers[0]
+    let casa = Pers[1]
+    let foto = Pers[2]
+    document.getElementById("Nombre").innerHTML = `Personaje: ${nombre}`
+    document.getElementById("imgPerso").innerHTML = `<img id="imgPerso" src="${foto}" class="imgPerso">`
+    document.getElementById('Casa').innerHTML = `${casa}`
+}
+
+
+
+
+
+
+
